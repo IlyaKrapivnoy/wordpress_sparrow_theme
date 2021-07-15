a95d1eb665548bc1e9b103afe0d51b2380da66af
ilya.krapivnoy <ilya.krapivnoy@gmail.com>
2021-07-15 15:46:37 +0300

    fix styles issues

--- functions.php
+++ functions.php
@@ -4,16 +4,16 @@ add_action('wp_footer', 'scripts_theme');
 add_action('after_setup_theme', 'theme_register_nav_menu');
 
 function theme_register_nav_menu() {
-	register_nav_menu( 'top', 'меню в шапке' );
+	register_nav_menu( 'top', 'Меню в шапке' );
 }
 
 
 function style_theme() {
     wp_enqueue_style('style', get_stylesheet_uri());
-    wp_enqueue_style('add_styles', get_template_directory_uri() . '/assets/css/default.css' );
-    wp_enqueue_style('add_styles', get_template_directory_uri() . '/assets/css/layout.css' );
-    wp_enqueue_style('add_styles', get_template_directory_uri() . '/assets/css/media-queries.css' );
-    wp_enqueue_style('add_styles', get_template_directory_uri() . '/assets/css/fonts.css' );
+    wp_enqueue_style('default', get_template_directory_uri() . '/assets/css/default.css' );
+    wp_enqueue_style('layout', get_template_directory_uri() . '/assets/css/layout.css' );
+    wp_enqueue_style('queries', get_template_directory_uri() . '/assets/css/media-queries.css' );
+    wp_enqueue_style('fonts', get_template_directory_uri() . '/assets/css/fonts.css' );
 }
   
 function scripts_theme() {
