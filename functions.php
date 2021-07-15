d870f64bc910b7b0b3537a71c485fed2248dd269
ilya.krapivnoy <ilya.krapivnoy@gmail.com>
2021-07-15 11:59:20 +0300

    add menu

---
+++ functions.php
@@ -0,0 +1,23 @@
+<?php 
+add_action('wp_enqueue_scripts', 'style_theme');
+add_action('wp_footer', 'scripts_theme');
+add_action('after_setup_theme', 'theme_register_nav_menu');
+
+function theme_register_nav_menu() {
+	register_nav_menu( 'top', 'меню в шапке' );
+}
+
+
+function style_theme() {
+    wp_enqueue_style('style', get_stylesheet_uri());
+    wp_enqueue_style('add_styles', get_template_directory_uri() . '/assets/css/default.css' );
+    wp_enqueue_style('add_styles', get_template_directory_uri() . '/assets/css/layout.css' );
+    wp_enqueue_style('add_styles', get_template_directory_uri() . '/assets/css/media-queries.css' );
+    wp_enqueue_style('add_styles', get_template_directory_uri() . '/assets/css/fonts.css' );
+}
+  
+function scripts_theme() {
+    wp_enqueue_scripts('init', get_template_directory_uri() . '/assets/js/init.js');
+    wp_enqueue_scripts('doubletaptogo', get_template_directory_uri() . '/assets/js/doubletaptogo.js');
+    wp_enqueue_scripts('modernizr', get_template_directory_uri() . '/assets/js/modernizr.js');
+}
