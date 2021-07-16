c02ae35838b92a3bc6fbe9a61213f139878f5c94
ilya.krapivnoy <ilya.krapivnoy@gmail.com>
2021-07-16 18:13:59 +0300

    add navigation

--- category.php
+++ category.php
@@ -29,49 +29,44 @@
                     <article class="post">
                     <div class="entry-header cf">
                         <h1><a href="<?php the_permalink(); ?>" title=""><?php the_title(); ?></a></h1>
-
                         <p class="post-meta">
-
-                            <time class="date" datetime="2014-01-14T11:24"><?php the_time('F jS, Y') ?> </time>
-                            /
+                            <time class="date"><?php the_time('F jS, Y') ?></time>
                             <span class="categories">
                                 <?php the_tags( ' ', ' / '); ?>
                             </span>
-
                         </p>
-
                     </div>
-
                     <div class="post-thumb">
-                            <a href="<?php the_permalink(); ?>" title=""><?php the_post_thumbnail('post_thumb') ?></a>
+                        <a href="<?php the_permalink(); ?>" title="">
+                            <?php the_post_thumbnail('post_thumb') ?>
+                        </a>
                     </div>
-
                     <div class="post-content">
-
                         <?php the_excerpt(); ?>
-
                     </div>
-
                     </article> <!-- post end -->
-                    <?php }
-                } ?>
+                    <?php } // конец while ?>
+                    <!-- Pagination -->
+                    <?php the_posts_pagination(); ?>
 
-            <!-- Pagination -->
-            <nav class="col full pagination">
-  			      <ul>
-                  <li><span class="page-numbers prev inactive">Prev</span></li>
-  				      <li><span class="page-numbers current">1</span></li>
-  				      <li><a href="#" class="page-numbers">2</a></li>
-                  <li><a href="#" class="page-numbers">3</a></li>
-                  <li><a href="#" class="page-numbers">4</a></li>
-                  <li><a href="#" class="page-numbers">5</a></li>
-                  <li><a href="#" class="page-numbers">6</a></li>
-                  <li><a href="#" class="page-numbers">7</a></li>
-                  <li><a href="#" class="page-numbers">8</a></li>
-                  <li><a href="#" class="page-numbers">9</a></li>
-  				      <li><a href="#" class="page-numbers next">Next</a></li>
-  			      </ul>
-  		      </nav>
+                    <!-- <nav class="col full pagination">
+                        <ul>
+                            <li><span class="page-numbers prev inactive">Prev</span></li>
+                            <li><span class="page-numbers current">1</span></li>
+                            <li><a href="#" class="page-numbers">2</a></li>
+                            <li><a href="#" class="page-numbers">3</a></li>
+                            <li><a href="#" class="page-numbers">4</a></li>
+                            <li><a href="#" class="page-numbers">5</a></li>
+                            <li><a href="#" class="page-numbers">6</a></li>
+                            <li><a href="#" class="page-numbers">7</a></li>
+                            <li><a href="#" class="page-numbers">8</a></li>
+                            <li><a href="#" class="page-numbers">9</a></li>
+                            <li><a href="#" class="page-numbers next">Next</a></li>
+                        </ul>
+                    </nav> -->
+                <?php } ?> 
+
+            
 
         </div> <!-- Primary End-->
 
